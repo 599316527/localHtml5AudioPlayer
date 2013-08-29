3d936a74df042968d2c32b2b189156e5d83619de
Kyle <admin@hk1229.cn>
2013-08-29 14:38:42 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-localHtml5AudioPlayer
+Local Html5 Audio Player
 =====================
 
-A local html5 audio player which works in webkit browsers
+A local html5 audio player which works in **webkit** browsers.
+
+## Usage
+
+* Open index.html with a webkit browser.
+* Drag audio files into the dashed border area.
+* Play audios.
+* Control the player by clicking icons, dragging sliders or pressing shortcut keys.
+
+## Notice
+
+* The app only works properly in browsers with **webkit** core.
+* Audio formats which the player support depends on the html element <audio>.
+* Press the key "?" to show/hide the list of shortcut keys.
+
+Have a nice day!
---
+++ js/main.js
@@ -0,0 +1,49 @@
+var player = new Player({
+    'player' : document.querySelector('#audio-player'),
+    'diyplayer' : {
+        'playBtn' : document.querySelector('#play-btn'),
+        'switchBtn' : {
+            'next' : document.querySelector('#next-btn'),
+            'prev' : document.querySelector('#prev-btn')
+        },
+        'name' : document.querySelector('#audio-name'),
+        'volume' : document.querySelector('#audio-volume'),
+        'process' : document.querySelector('#audio-process'),
+        'time' : {
+            'current' : document.querySelector('#audio-time-current'),
+            'total' : document.querySelector('#audio-time-total')
+        },
+        'loop' : {
+            'single' : document.querySelector('#loop-single')
+        },
+        'help' : {
+            'page' : document.querySelector('#pop-win-wrapper'),
+            'closeBtn' : document.querySelector('#pop-win-wrapper .close-btn')
+        }
+    }
+});
+
+
+var lst = new List({
+    'dropPlaceWrapper' : document.querySelector('#drop-place-wrapper'),
+    'dropPlace' : document.querySelector('#drop-place'),
+    'listPlace' : document.querySelector('#audio-list'),
+    'audioPlayer' : document.querySelector('#audio-player'),
+    'allowAudioTypes' : ['audio/mpeg', 'audio/mp3', 'audio/x-m4a', 'audio/x-wav'],
+    'listTpl' : document.querySelector('#audio-list').innerHTML,
+    'rename' : function (name) {
+        return name.replace(/^\d+\b|\.\w{2,5}$/g, '');
+    },
+    'finishAudioLoad' : function (id, name) {
+        player.finishLoad.call(player, id, name.replace(/\.\w{2,5}$/g, ''));
+    },
+    'showLoading' : function () {
+        player.diy.name.innerHTML = 'Loading...';
+    }
+});
+
+player.setAudioListApi({
+    'goto' : function (id) {
+        lst.get.call(lst, id);
+    }
+});
